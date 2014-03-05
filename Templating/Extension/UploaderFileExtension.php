edc8d276eb376dffbea68bb5ca535fbcc7fd9251
Rob Frawley 2nd <rfrawley@scribenet.com>
2014-03-05 12:08:02 -0500

    fix for andrew

--- Templating/Extension/UploaderFileExtension.php
+++ Templating/Extension/UploaderFileExtension.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Scribe\FileUploaderBundle\Twig;
+namespace Scribe\FileUploaderBundle\Templating\Extension;
 
 use Symfony\Component\DependencyInjection\ContainerInterface,
     Symfony\Component\DependencyInjection\ContainerAwareInterface;
