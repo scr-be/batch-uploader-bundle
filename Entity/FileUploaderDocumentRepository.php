d4e6d342d55ee1ece21ab16bc8ec2a8e77a0fa2d
Rob Frawley 2nd <rfrawley@scribenet.com>
2014-03-09 20:51:34 -0400

    added repository call to get file count

--- Entity/FileUploaderDocumentRepository.php
+++ Entity/FileUploaderDocumentRepository.php
@@ -9,4 +9,19 @@ use Doctrine\ORM\EntityRepository;
  */
 class FileUploaderDocumentRepository extends EntityRepository
 {
+	/**
+	 * @return integer
+	 */
+	public function findCountByEditId($editId)
+	{
+		$q = $this
+			->createQueryBuilder('f')
+    		->select('count(f.id)')
+    		->where('f.editId = :editId')
+			->setParameter('editId', $editId)
+    		->getQuery()
+    	;
+
+    	return $q->getSingleScalarResult();
+	}
 }
