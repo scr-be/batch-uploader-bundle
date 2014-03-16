0de6d20577187f6cd22608b14277de7419965c7a
Rob Frawley 2nd <rfrawley@scribenet.com>
2014-03-16 19:25:08 -0400

    updated repository class

--- Entity/FileUploaderDocumentRepository.php
+++ Entity/FileUploaderDocumentRepository.php
@@ -24,4 +24,37 @@ class FileUploaderDocumentRepository extends EntityRepository
 
     	return $q->getSingleScalarResult();
 	}
+
+	/**
+	 * @return integer
+	 */
+	public function findImagesByEditId($editId, $exts = ['jpeg', 'jpg', 'png', 'bmp', 'tiff', 'tif'])
+	{
+		$qb = $this
+			->createQueryBuilder('f')
+		;
+
+		$whereOrX = [];
+		for ($i = 0; $i < count($exts); $i++) {
+			$whereOrX[] = $qb->expr()->eq('f.extension', ':ext'.$i);
+		}
+		$exp = $qb->expr();
+		$orx = call_user_func_array([$exp, 'orX'], $whereOrX);
+
+		$qb
+    		->where('f.editId = :editId')
+    		->andWhere($orx)
+			->setParameter('editId', $editId)
+		;
+
+		for ($i = 0; $i < count($exts); $i++) {
+			$qb->setParameter('ext'.$i, $exts[$i]);
+		}
+
+		$q = $qb
+    		->getQuery()
+    	;
+
+    	return $q->getResult();
+	}
 }
